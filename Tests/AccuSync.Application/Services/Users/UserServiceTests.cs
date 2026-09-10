03a874bfda715a24421206d0877aee851d202b87
paramasivam-soliton <paramasivam.g@solitontech.com>
2026-09-10 14:32:16 +0530

    test pass

--- Tests/AccuSync.Application/Services/Users/UserServiceTests.cs
+++ Tests/AccuSync.Application/Services/Users/UserServiceTests.cs
@@ -62,7 +62,7 @@ namespace AccuSync.Application.Tests.Services.Users
             bool success = await _userService.CreateUserAsync(user);
 
             Assert.True(success);
-            Assert.NotEqual("Admi", persisted.AccountName);
+            Assert.NotEqual("Admin", persisted.AccountName);
         }
 
         [Fact]
